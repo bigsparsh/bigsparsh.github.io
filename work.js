b2d34944d2f7dca484da35c2e1aead1242736916
bigsparsh <86356748+bigsparsh@users.noreply.github.com>
2023-04-18 16:41:10 +0530

    Update work.js

--- work.js
+++ work.js
@@ -1,23 +1,9 @@
+// 						TOP BUTTTON FUNCTIONALITY						
 
-function menu(){
-	document.querySelector('.menu-bar').classList.toggle('appear');
-}
-
-let box = document.querySelector('.cursor').style;
-let x, y;
-
-document.addEventListener('mousemove', (e) =>{
-	x = e.clientX;
-	y = e.clientY;
-	box.setProperty("left", (x-20)+"px");
-	box.setProperty("top", (y-20)+"px");
+document.addEventListener('scroll', ()=>{
+	document.querySelector('.top-btn').style.setProperty('display' , 'flex');
 })
 
-document.addEventListener('mousedown', (e) =>{
-	box.setProperty("transform","scale(0.5)");
-	box.setProperty("background","white");
-})
-document.addEventListener('mouseup', (e) =>{
-	box.setProperty("transform","scale(1)");
-	box.setProperty("background","none");
+document.querySelector('.top-btn').addEventListener('click', () =>{
+	location.href = '#sec1';
 })
