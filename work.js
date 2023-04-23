44dd49de4e3ac3451216a6b426dd705638f03883
bigsparsh <86356748+bigsparsh@users.noreply.github.com>
2023-04-23 21:19:22 +0530

    Update work.js

--- work.js
+++ work.js
@@ -7,3 +7,23 @@ document.addEventListener('scroll', ()=>{
 document.querySelector('.top-btn').addEventListener('click', () =>{
 	location.href = '#sec1';
 })
+
+//					REGISTER FORM FUNCTIONALITY
+
+let btn = document.querySelector('.sml-btn');
+let closeBtn = document.querySelector('.close-btn');
+btn.addEventListener('click', ()=>{
+	appearRegister();
+})
+closeBtn.addEventListener('click', ()=>{
+	disappearRegister();
+})
+
+let regForm = document.querySelector('.form-back');
+
+function appearRegister(){
+	regForm.style.display = 'flex';
+}
+function disappearRegister(){
+	regForm.style.display = 'none';
+}
