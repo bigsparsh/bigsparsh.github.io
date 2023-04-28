6eb4704d4c1d5e6cf3f63755d6ebc156dba29cd6
bigsparsh <86356748+bigsparsh@users.noreply.github.com>
2023-04-28 21:36:31 +0530

    Update work.js

--- work.js
+++ work.js
@@ -1,29 +1,29 @@
-// 						TOP BUTTTON FUNCTIONALITY						
-
-document.addEventListener('scroll', ()=>{
-	document.querySelector('.top-btn').style.setProperty('display' , 'flex');
+let hamMenu = document.querySelector('.ham-menu');
+let offMenu = document.getElementById('men');
+let x = 0;
+hamMenu.addEventListener('click', ()=>{
+	x++;
+	console.log(offMenu.style.left);
+	if(window.innerWidth < 600){
+		if(x%2===0){
+			offMenu.style.left = '-75%';
+		}
+		else{
+			offMenu.style.left = '0%';
+		}
+	}
+	else{
+		if(x%2===0){
+			offMenu.style.left = '-75%';
+		}
+		else{
+			offMenu.style.left = '0%';
+		}
+	}
 })
 
-document.querySelector('.top-btn').addEventListener('click', () =>{
-	location.href = '#sec1';
+window.addEventListener('resize', ()=>{
+	if(window.innerWidth > 1000){
+		offMenu.style.left = '-75%';
+	}
 })
-
-//					REGISTER FORM FUNCTIONALITY
-
-let btn = document.querySelector('.sml-btn');
-let closeBtn = document.querySelector('.close-btn');
-btn.addEventListener('click', ()=>{
-	appearRegister();
-})
-closeBtn.addEventListener('click', ()=>{
-	disappearRegister();
-})
-
-let regForm = document.querySelector('.form-back');
-
-function appearRegister(){
-	regForm.style.display = 'flex';
-}
-function disappearRegister(){
-	regForm.style.display = 'none';
-}
